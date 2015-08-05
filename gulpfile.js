a5f90c109ef06494c2626c101d955c36b0869d68
rSamokhin <poshliemail@googlemail.com>
2015-08-06 00:19:06 +0300

    label and selector

--- gulpfile.js
+++ gulpfile.js
@@ -48,10 +48,10 @@ gulp.task('html:build',function(){
 });
 gulp.task('js:build',function(){
     gulp.src(path.src.js)
-        .pipe(rigger())
+        .pipe(rigger())/*
         .pipe(sourcemaps.init())
         .pipe(uglify())
-        .pipe(sourcemaps.write())
+        .pipe(sourcemaps.write())*/
         .pipe(gulp.dest(path.build.js))
         .pipe(connect.reload())
 });
