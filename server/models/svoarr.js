ed93071ea60e863b94d84f707ccd0c8a11389ec9
rSamokhin <poshliemail@googlemail.com>
2015-08-10 01:09:56 +0300

    request saved data for debug purposes

---
+++ server/models/svoarr.js
@@ -0,0 +1,4 @@
+module.exports = function () {
+    var SvoArr = require('../data/svoarr.json');
+    return SvoArr;
+};
---
+++ server/models/svodep.js
@@ -0,0 +1,4 @@
+module.exports = function () {
+    var SvoDep = require('../data/svodep.json');
+    return SvoDep;
+};
